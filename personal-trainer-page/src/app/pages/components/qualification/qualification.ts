8a7c0bbce8166aae9bac075f68d966aef17335a7
Vitor Hugo Silva Gonçalves <vitorhugo662@hotmail.com>
2025-07-04 11:51:39 -0300

    update

---
+++ personal-trainer-page/src/app/pages/components/qualification/qualification.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qualification',
+  imports: [],
+  templateUrl: './qualification.html',
+  styleUrl: './qualification.scss'
+})
+export class Qualification {
+
+}
